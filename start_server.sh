66c5ba9e82f40a6d4ba90abbe09cbd442afc9d43
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-02-21 16:29:50 +0000

    Some clean up

--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+
+# Run script from its folder
 CWD=$PWD
+# Define repository location
 REPO=$GITLAB/sbc-prom.git
+# true for the cloud server
 DEPLOY=false
 
 if [ "$DEPLOY" == "true" ]; then
-
     docker stop $(docker ps -a -q)
     docker rm $(docker ps -a -q)
     docker rmi $(docker images -q)
@@ -24,8 +27,8 @@ cd $CWD
 docker build -t selprom .
 
 if [ "$DEPLOY" == "true" ]; then
-
     docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 fi
 
+# Run contained with user's uid to avoid permission issues when updating the repository
 docker run -u `id -u $USER` --name selprom -d -p 7700:7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $CWD/sbc-prom:/selprom selprom 
