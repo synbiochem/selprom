372af69ce96658fedf8a32eb428ae3b1fcddf6bd
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-09-21 11:43:27 +0100

    Port change

--- start_server.sh
+++ start_server.sh
@@ -30,7 +30,7 @@ docker build -t selprom .
 if [ "$DEPLOY" == "true" ]; then
     #    docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
     # Run container with user's uid to avoid permission issues when updating the repository
-    docker run -u `id -u $USER` --name selprom -d -p :7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selprom.synbiochem.co.uk  -v $CWD/sbc-prom:/selprom selprom 
+    docker run -u `id -u $USER` --name selprom -d -p 7700:7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selprom.synbiochem.co.uk  -v $CWD/sbc-prom:/selprom selprom 
 else
     # Run container with user's uid to avoid permission issues when updating the repository
     docker run -u `id -u $USER` --name selprom -d -p 7700:7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $CWD/sbc-prom:/selprom selprom 
