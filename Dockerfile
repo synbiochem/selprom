0a316072f6027b826054ada089b5d73c2a83153f
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-02-20 12:43:21 +0000

    Update proxy

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@
 # build with: "sudo docker build -t selprom ."
 
 # Install basic image
-FROM continuumio/anaconda3:5.0.1
+FROM continuumio/anaconda3:5.2.0
 
 # Install additional tools
 RUN conda install -c conda-forge flask-restful=0.3.6
--- start_server.sh
+++ start_server.sh
@@ -32,7 +32,7 @@ docker build -t selprom .
 
 # Run container
 if [ "$DEPLOY" == "true" ]; then
-    docker run --name nginx-proxy -d -p 8080:8080 -p 80:7700 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+    docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
     # Run container with user's uid to avoid permission issues when updating the repository
     docker run -u `id -u $USER` --name selprom -d -p 7700:7700 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -e VIRTUAL_HOST=selprom.synbiochem.co.uk  -v $CWD/sbc-prom:/selprom selprom 
 else
